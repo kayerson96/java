9a07e7a649fbe39574f7a858ecd2ad63d4a45209
kayerson96 <108358349+kayerson96@users.noreply.github.com>
2023-03-12 19:29:58 -0500

    se esta alistando los mensajes

--- src/main/java/com/platzi/MensajesDAO.java
+++ src/main/java/com/platzi/MensajesDAO.java
@@ -1,15 +1,57 @@
 package com.platzi;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class MensajesDAO {
-    public static void  crearMensajeDB(Mensajes mensajes){
+    public static void  crearMensajeDB(Mensajes mensaje){
+        Conexion db_connect = new Conexion();
+            try (Connection conexion = db_connect.get_connection()){
+                PreparedStatement ps = null;
+                try {
+                    String query = "INSERT INTO mensajes (mensaje, autor_mensaje) VALUES (?,?)";
+                    ps =  conexion.prepareStatement(query);
+                    ps.setString(1, mensaje.getMensaje());
+                    ps.setString(2, mensaje.getAutor());
+                    ps.executeUpdate();
+                    System.out.println("el mensaje se creo correctamente");
+                }catch (SQLException ex){
+                    System.out.println(ex);
+
+                }
+
+            }catch (SQLException e){
+                System.out.println(e);
+            }
 
     }
 
     public static void  leerMensajeDB(){
+        Conexion db_connect = new Conexion();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
 
+        try (Connection conexion = db_connect.get_connection()){
+        String query = "SELECT * FROM mensajes";
+        ps = conexion.prepareStatement(query);
+        rs = ps.executeQuery();
+
+        while (rs.next()){
+            System.out.println("id"+ rs.getInt("id_mensaje"));
+            System.out.println("mensaje:" + rs.getString("mensaje"));
+            System.out.println("autor:"+ rs.getString("autor_mensaje"));
+            System.out.println("fecha:"+ rs.getString("fecha_mensaje"));
+            System.out.println("");
+        }
+        } catch (SQLException e){
+            System.out.println("no se pudieron recuperar los mensajes");
+        System.out.println(e);
+    }
     }
 
-    public static void borrarMensaje(int id_mensaje) {
+    public static void borrarMensajeDB(int id_mensaje) {
 
     }
     public static void actualizarMensajeDB(Mensajes mensaje) {
--- src/main/java/com/platzi/MensajesService.java
+++ src/main/java/com/platzi/MensajesService.java
@@ -1,8 +1,19 @@
 package com.platzi;
 
+import java.util.Scanner;
+
 public class MensajesService {
         public static void crearMensaje() {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("escribe tu mensaje");
+            String mensaje = sc.nextLine();
+            System.out.println("ingresa tu nombre");
+            String nombre = sc.nextLine();
 
+        Mensajes registro = new Mensajes();
+        registro.setMensaje(mensaje);
+        registro.setAutor(nombre);
+        MensajesDAO.crearMensajeDB(registro);
         }
 
         public static void listarMensajes(){
